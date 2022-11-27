9c047204586e53ab0b516a3bf7cce536b923c0da
Garvit-Singh <garvitjadon003@gmail.com>
2022-11-28 01:49:00 +0530

    upload document test route made

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ from .routes.main import main
 # create flask app
 app = Flask(__name__)
 
+# config for file 
+MAX_CONTENT_ALLOWED = 16 * 1000 * 1000
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_ALLOWED
+
 # config database & start mongose
 app.config['MONGO_URI'] = f'mongodb+srv://{usr}:{pwd}@cluster0.nbpt6ub.mongodb.net/{db}?retryWrites=true&w=majority'
 mongo.init_app(app)
--- routes/main.py
+++ routes/main.py
@@ -1,9 +1,17 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, request
+from werkzeug.utils import secure_filename
 
 from ..mongodb.model import Accounts
-from ..mongodb.mongo_connect import mongo
 
 main = Blueprint('main', __name__)
+
+UPLOAD_FOLDER = './crypto/uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @main.route('/', methods=['POST'])
 def login():
@@ -12,6 +20,23 @@ def login():
 @main.route('/getkeys', methods=['GET'])
 def getkeys():
     return Accounts().getkey()
+
+@main.route('/upload', methods=['POST'])
+def upload():
+    
+    file = request.files['document']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return 'No selected file'
+        
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return 'File Saved to Uploads!'
+
+    # DO NOT FORGET TO DELETE THE DOCUMENT ONCE UPLOADED AND ENCRYPTED
+    return 'Uploaded Successfully!'
 
 @main.route('/authority')
 def institute_authority():
